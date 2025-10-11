3336b087b3d834a40819287e660e9dafd8817f82
vais2004 <vkawale2004@gmail.com>
2025-10-11 22:18:45 +0530

    update feat : created an routes page for the Address

---
+++ Routes/Address.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const router = express.Router();
+const Address = require("../models/address.models");
+
+router.post("/add-address", async (req, res) => {
+  try {
+    const {
+      userEmail,
+      fullName,
+      mobile,
+      house,
+      area,
+      town,
+      pincode,
+      state,
+      isDefault,
+    } = req.body;
+
+    if (
+      !userEmail ||
+      !fullName ||
+      !mobile ||
+      !house ||
+      !area ||
+      !town ||
+      !pincode
+    ) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Missing required fields" });
+    }
+
+    if (isDefault) {
+      await Address.updateMany({ userEmail }, { $set: { isDefault: false } });
+    }
+
+    const newAddress = new Address({
+      userEmail,
+      fullName,
+      mobile,
+      house,
+      area,
+      town,
+      pincode,
+      state,
+      isDefault,
+    });
+
+    await newAddress.save();
+    res
+      .status(201)
+      .json({ message: "Address added successfully", address: newAddress });
+  } catch (error) {
+    console.error("Error adding address:", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/get-addresses", async (req, res) => {
+  try {
+    const { userEmail } = req.body;
+    if (!userEmail) {
+      res.status(400).json({ error: "email required" });
+    }
+
+    const addresses = await Address.find({ userEmail });
+    res.json({ addresses });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.delete("/delete-address/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Address.findByIdAndDelete(id);
+    res.json({ success: true, message: "Address deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/set-default", async(req,res)=>{
+    try{
+
+    }catch(error){
+        
+    }
+})
